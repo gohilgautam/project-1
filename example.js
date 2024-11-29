a5da5e2a683edfbf2667000f660febc93424cbc8
gohilgautam <gohilgautam2405@gmail.com>
2024-11-29 18:57:42 +0530

    example

---
+++ example.js
@@ -0,0 +1,27 @@
+
+    // 1. An electricity board charges the following rates to domestic users to discourage large comsumption of energy for the first 100 units 60p per unit for the next 200 units 80p per unit for the next 300 units 90p per unit All users are charged a minimun of Rs.50. if the total amount is more than 300.00 then an additional surcharge of 15% is added.
+
+
+
+
+
+    // 2. check wheaeher a person is eligible for voting or not?
+
+    let age;
+    age = +prompt("enter your age")
+
+    if(age <= 18){
+        console.log("you are not eligible for voting")
+    }
+        else if(age <= 0){
+            console.log("please Enter valid age num.")
+        }
+    else{
+        console.log("you are eligible for voting")
+    }
+
+
+    // 6.
+
+    let x;
+    let y;
