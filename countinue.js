6d915ff140197ae3bf00a84f5aa3dcdd86669aab
gohilgautam <gohilgautam2405@gmail.com>
2024-12-03 19:00:15 +0530

    12-3-2024

---
+++ countinue.js
@@ -0,0 +1,85 @@
+//1. print out 10 to 1
+
+// for(let i=10; i>=1; i--){
+//     console.log(i);
+// }
+
+// ----------------------------------------------------
+
+//2. print out 50 to 79
+
+// for(let i=50; i<=79; i++){
+//     console.log(i);
+// }
+
+// ----------------------------------------------------
+
+//3. enter n and print 1 to n
+
+// let n = +prompt("Enter a number: ");
+
+// for(let i=1; i<=n; i++){
+//     console.log(i);
+// }
+
+// ----------------------------------------------------
+
+//4. enter n and print -n to 1
+
+// let n = +prompt("enter your number");
+
+// for(let i=-n; i<=1; i++){
+//     console.log(i);
+// }
+
+// ----------------------------------------------------
+
+//5. enter n and print -n to n
+
+// let n = +prompt("enter your number");
+
+// for(let i=-n; i<=n; i++){
+//     console.log(i);
+// }
+
+// ----------------------------------------------------
+
+//6. enter n and print out odd number upto n
+
+// let n = +prompt("enter your number");
+
+// for(let i=1; i<=n; i+=2){
+//     console.log(i);
+// }
+
+// ----------------------------------------------------
+
+//7. enter n and print out even number upto n
+
+// let n = +prompt("enter your number");
+
+// for(let i=2; i<=n; i+=2){
+//     console.log(i);
+// }
+
+// ----------------------------------------------------
+
+//8. series print out 1, 4, 3, 16, 5,......
+
+// let n = +prompt("enter your number");
+//   let odd = 1;
+//   let even = 2;
+
+//   for (let i = 1; i <= n; i++){
+//     if (i % 2 !== 0) { 
+//       console.log(odd);
+//       odd += 2;
+//     }
+//     else { 
+//       console.log(even * even);
+//       even += 2; 
+//     }
+//   }
+
+// ----------------------------------------------------
+
