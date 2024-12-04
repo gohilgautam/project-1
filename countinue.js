092b8b6956cfbe0f7f4f2797e166d988d3e1547c
gohilgautam <gohilgautam2405@gmail.com>
2024-12-04 18:22:01 +0530

    4-12-2024

--- countinue.js
+++ countinue.js
@@ -66,20 +66,101 @@
 
 //8. series print out 1, 4, 3, 16, 5,......
 
-// let n = +prompt("enter your number");
-//   let odd = 1;
-//   let even = 2;
+// let n = +prompt("Enter a Number :");
+// let x = 0; y = 1;
+// let z;
 
-//   for (let i = 1; i <= n; i++){
-//     if (i % 2 !== 0) { 
-//       console.log(odd);
-//       odd += 2;
+//Method : 1
+// for(let i = 0; i <= e; i++){
+//     console.log(x);
+
+//     z = x + y;
+//     x = y;
+//     y = z;
+// }
+
+
+//Method : 2
+// for(let i = 1; i <= e; i++){
+//     if(i == 1){
+//         console.log(x);
+//     }else if(i == 2){
+//         console.log(y);
+//     }else{
+//         z = x + y;
+//         x = y;
+//         y = z;
+//         console.log(z);
 //     }
-//     else { 
-//       console.log(even * even);
-//       even += 2; 
-//     }
-//   }
+// }
 
 // ----------------------------------------------------
 
+// 10. series print out 1, 4, 12, 32, ......
+
+// let a=1,  i;
+// let n = +prompt("enter your number");
+// for (i=1; i<=n; i++){
+//     console.log(a*i);
+//     a*=2;
+// }
+
+// ----------------------------------------------------
+
+// 11. find out factorial of given number
+
+// let n = +prompt("enter your number");
+// let num = 1;
+// for (let i = 1; i <= n; i++) {
+//     num *= i;
+//     console.log(num);
+//     }
+
+// ----------------------------------------------------
+
+// 12. enter n and print out tota; number's sum
+
+// let n = +prompt("enter your number");
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+// ----------------------------------------------------
+
+// 13. enter n and print out even number's sum
+
+// let n = +prompt("enter your number");
+// let sum = 0;
+
+// for (let i = 1; i <= n; i+=2) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+// ----------------------------------------------------
+
+// 14. enter n and print out odd number's sum
+
+// let n = +prompt("enter your number");
+// let sum = 0;
+
+// for(let i=2; i<=n; i+=2){
+//     sum += i;
+//     console.log(sum);
+// }
+
+// ----------------------------------------------------
+
+// 15. enter n and print out sum of digit of number
+
+let n = +prompt("enter your number");
+let sum = 0;
+let num = n;
+
+for(let i=1; i<=num; i++){
+    sum += num % 10;
+    console.log(sum);
+    }
